29e1a6fdda76dfc16bb60c5a89d0c8ee72036706
takiyoto <pedromiguelmvs@hotmail.com>
2019-10-15 18:46:45 -0300

    Mudanças no servidor

--- servidor.js
+++ servidor.js
@@ -5,6 +5,8 @@ var path = require('path');
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/assets/css/main.css');
+    res.sendFile(__dirname + 'images/9132pawprint.ico');
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
