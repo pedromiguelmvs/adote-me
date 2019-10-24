512738914b10d85929742d3aa0eaafb0adbcd358
takiyoto <pedromiguelmvs@hotmail.com>
2019-10-24 10:22:02 -0300

    Mudanças no servidor.

--- servidor.js
+++ servidor.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 var path = require('path');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
-    res.sendFile(__dirname + '/assets/css/main.css');
-    res.sendFile(__dirname + 'images/9132pawprint.ico');
+    //res.sendFile(__dirname + '/assets/css/main.css');
+    //res.sendFile(__dirname + '/images/9132pawprint.ico');
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
